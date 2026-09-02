ece8c33a345803837b63d6a8f0ae7b371a2c75c8
certifiedTboy <etosin70@gmail.com>
2026-09-02 16:29:29 +0100

    fix app url

--- src/app.ts
+++ src/app.ts
@@ -13,26 +13,25 @@ class ExpressApp extends App {
       res.json({ message: "Server is live" });
     });
 
-    this.app.use(
-      "/api/v1/api-docs",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerSpec),
-    );
+    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-    this.app.use("/api/v1/users", new UserRoutes().routes);
-    this.app.use("/api/v1/auth", new AuthRoutes().routes);
-    this.app.use("/api/v1/services", new ServiceRoutes().routes);
-    this.app.use("/api/v1/messages", new MessageRoutes().routes);
+    this.app.use("/users", new UserRoutes().routes);
+    this.app.use("/auth", new AuthRoutes().routes);
+    this.app.use("/services", new ServiceRoutes().routes);
+    this.app.use("/messages", new MessageRoutes().routes);
   }
 }
 
-const newApp = new ExpressApp({
-  origin: [
-    "http://localhost:5173",
-    "https://ajike-pest-control-tau.vercel.app",
-  ],
-  credentials: true,
-}, express());
+const newApp = new ExpressApp(
+  {
+    origin: [
+      "http://localhost:5173",
+      "https://ajike-pest-control-tau.vercel.app",
+    ],
+    credentials: true,
+  },
+  express(),
+);
 const { app } = newApp;
 
 export default app;
