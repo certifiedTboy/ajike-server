f3ef723138c13ec55b45f8c0f41e2d8e2e8f2274
certifiedTboy <etosin70@gmail.com>
2026-09-02 18:58:06 +0100

    udpate app config

--- src/app.ts
+++ src/app.ts
@@ -13,12 +13,16 @@ class ExpressApp extends App {
       res.json({ message: "Server is live" });
     });
 
-    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    this.app.use(
+      "/api/v1/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(swaggerSpec),
+    );
 
-    this.app.use("/users", new UserRoutes().routes);
-    this.app.use("/auth", new AuthRoutes().routes);
-    this.app.use("/services", new ServiceRoutes().routes);
-    this.app.use("/messages", new MessageRoutes().routes);
+    this.app.use("/api/v1/users", new UserRoutes().routes);
+    this.app.use("/api/v1/auth", new AuthRoutes().routes);
+    this.app.use("/api/v1/services", new ServiceRoutes().routes);
+    this.app.use("/api/v1/messages", new MessageRoutes().routes);
   }
 }
 
